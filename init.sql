11cfb798294ae1ea686a330e5cb9ce98c5cf5bc9
Felix <larssonsfelix@gmail.com>
2026-02-05 15:27:00 +0100

    add tables to init.sql

--- init.sql
+++ init.sql
@@ -4,16 +4,33 @@ CREATE TABLE accessories (
   -- pullrate is int between 0 to 100
   pullrate DECIMAL NOT NULL,
   equipped BOOLEAN NOT NULL,
+  theme_id INT NOT NULL,
   -- luck is int between -100 to 100 can be null
-  luck DECIMAL
+  passive_income int,
+  luck DECIMAL,
+  CONSTRAINT fk_theme
+    FOREIGN KEY (theme_id)
+      REFERENCES themes(id) ON DELETE CASCADE
+);
+
+CREATE TABLE themes (
+  id SERIAL NOT NULL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL UNIQUE
 );
 
 ALTER TABLE accessories ADD COLUMN equipped BOOLEAN NOT NULL;
 
 INSERT INTO
-  accessories ( name, pullrate, equipped, luck )
+  accessories ( name, pullrate, equipped, theme_id, passive_income, luck )
 VALUES 
-  ( 'sunglasses', 50, true, 1.2 );
+  ( 'faceware', 50, true, 1, 20, 1 ),
+  ( 'faceware', 50, true, 2, 0, 1.5 );
+
+INSERT INTO
+  themes ( name )
+VALUES 
+  ( 'viking' ),
+  ( 'samurai' );
 
 DELETE FROM accessories WHERE id = 1;
 
