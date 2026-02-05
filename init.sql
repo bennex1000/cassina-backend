a9c893a257d5cb5e400444fb191ec69041840f9f
Felix <larssonsfelix@gmail.com>
2026-02-05 14:18:27 +0100

    change init.sql

--- init.sql
+++ init.sql
@@ -3,12 +3,23 @@ CREATE TABLE accessories (
   name VARCHAR(50) NOT NULL,
   -- pullrate is int between 0 to 100
   pullrate DECIMAL NOT NULL,
+  equipped BOOLEAN NOT NULL,
   -- luck is int between -100 to 100 can be null
   luck DECIMAL
 );
 
-INSERT INTO
-  accessories ( name, pullrate, luck )
-VALUES 
-  ( 'sunglasses', 50, 1.2 );
+ALTER TABLE accessories ADD COLUMN equipped BOOLEAN NOT NULL;
 
+INSERT INTO
+  accessories ( name, pullrate, equipped, luck )
+VALUES 
+  ( 'sunglasses', 50, true, 1.2 );
+
+DELETE FROM accessories WHERE id = 1;
+
+UPDATE accessories
+SET
+  luck = 1.2
+WHERE id = 1;
+
+SELECT * FROM accessories;
